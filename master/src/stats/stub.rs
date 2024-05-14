8cf76e55acd5df104dffa61fa0dc40351e812624
Denis Drakhnia <numas13@gmail.com>
2024-05-14 11:38:32 +0300

    master: add stats feature flag

---
+++ master/src/stats/stub.rs
@@ -0,0 +1,17 @@
+use crate::config::StatConfig;
+
+#[derive(Default)]
+struct Counters;
+
+pub struct Stats;
+
+impl Stats {
+    pub fn new(_: StatConfig) -> Self { Self }
+    pub fn update_config(&mut self, _: StatConfig) {}
+    pub fn clear(&self) {}
+    pub fn servers_count(&self, _: usize) {}
+    pub fn on_server_add(&self) {}
+    pub fn on_server_del(&self) {}
+    pub fn on_query_servers(&self) {}
+    pub fn on_error(&self) {}
+}

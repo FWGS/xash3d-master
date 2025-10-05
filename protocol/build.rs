1979e3a1b1c588db11b63dad8ddc2561dae66fe0
Denis Drakhnia <numas13@gmail.com>
2025-10-05 09:35:38 +0300

    protocol: remove build script

--- protocol/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let ac = autocfg::new();
-    println!("cargo:rustc-check-cfg=cfg(has_doc_auto_cfg)");
-    if ac.probe_raw("#![feature(doc_auto_cfg)]").is_ok() {
-        println!("cargo:rustc-cfg=has_doc_auto_cfg");
-    }
-}
